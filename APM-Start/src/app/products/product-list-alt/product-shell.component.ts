56aca325c76fea1574b43642fb818277e8f69622
Bernhard Dorn <mail@bernharddorn.de>
2023-04-09 14:01:56 +0200

    Module 11

--- APM-Start/src/app/products/product-list-alt/product-shell.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-shell.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SupplierService } from 'src/app/suppliers/supplier.service';
 
 @Component({
   templateUrl: './product-shell.component.html'
 })
 export class ProductShellComponent {
+  constructor(private supplierService: SupplierService) {
 
+  }
 }
